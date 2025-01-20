c1d50351636c4154789897b8d07a792ea74560b1
TAHAR Noureddine <engr.tahar.noureddine@gmail.com>
2025-01-20 15:29:35 +0100

    feat: Add home and login screens with router
    This commit introduces the `HomeScreen` and `LoginScreen` to the application. It also adds a new router (`RouterGenerator`) that handles navigation to these screens. Additionally, `NavigationRoutes` is introduced to define the routes for all the screens, like the home and login screens.

---
+++ lib/constant/screen_routes.dart
@@ -0,0 +1,13 @@
+class NavigationRoutes {
+  /// Dialogs --------------
+  static const progressDialog = "/progress-dialog";
+  static const errorDialog = "/error-dialog";
+  static const confirmDialog = "/confirm-dialog";
+  static const successDialog = "/success-dialog";
+
+  /// Screens --------------
+  static const home = "/";
+  static const login = "/login";
+  static const register = "/register";
+  static const forgotPassword = "/forgot-password";
+}
---
+++ lib/router.dart
@@ -0,0 +1,30 @@
+import 'package:echo/constant/screen_routes.dart';
+import 'package:echo/screen/home_screen.dart';
+import 'package:echo/screen/login_screen.dart';
+import 'package:flutter/material.dart';
+
+class RouterGenerator {
+  static Route? getRouter(RouteSettings routeSettings) {
+    switch (routeSettings.name) {
+      /// Dialogs ------------------------------------------------------------
+      // case progressDialogRoute:
+      //   return ProgressDialog(loadingText: routeSettings.arguments as String?);
+      // case confirmDialogRoute:
+      //   return ConfirmDialog(text: routeSettings.arguments as String?);
+      // case errorDialogRoute:
+      //   return ErrorDialog(contents: routeSettings.arguments as String);
+      /// Screens ------------------------------------------------------------
+      case NavigationRoutes.home:
+        return MaterialPageRoute(
+          settings: RouteSettings(name: routeSettings.name),
+          builder: (context) => const HomeScreen(),
+        );
+      case NavigationRoutes.login:
+        return MaterialPageRoute(
+          settings: RouteSettings(name: routeSettings.name),
+          builder: (context) => LoginScreen(),
+        );
+    }
+    return null;
+  }
+}
